76bcb2307b65f53d47e5664b275e0147cd241712
yxibng <yxibng@gmail.com>
2019-01-12 18:13:21 +0800

    modify

--- CustomNavigationBarAnimation/ViewController.m
+++ CustomNavigationBarAnimation/ViewController.m
@@ -8,16 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *CellMark = @"cell";
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UIView *topView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topViewHeight;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageWidth;
 
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellMark];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellMark];
+    cell.textLabel.text = @(indexPath.row).stringValue;
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    CGFloat offset = scrollView.contentOffset.y;
+    CGFloat height = self.topViewHeight.constant;
+    
+    
+    
+    if (offset > 0) {
+        if (height > 64) {
+            self.topViewHeight.constant = height - offset;
+            [scrollView setContentOffset:CGPointZero];
+            
+            
+            
+        }
+    } else {
+        if (height < 100) {
+            self.topViewHeight.constant = height - offset;
+        }
+    }
+    
+    CGFloat scale = 0.7 + 0.3 * (height -64)/(100-64);
+    self.imageWidth.constant = 50*scale;
+    
+    
+}
 @end
